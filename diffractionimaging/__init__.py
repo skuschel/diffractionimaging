c67cc63f05a01b14256b16f2a4ea2191791c8c0f
Stephan Kuschel <stephan.kuschel@gmail.com>
2022-07-26 17:13:16 +0200

    add correlations.py for incoherent diffraction imaging

--- diffractionimaging/__init__.py
+++ diffractionimaging/__init__.py
@@ -3,6 +3,7 @@
 from .clustersize import autofit_sphere
 from .atomicformfactors import *
 from .helpers import *
+from . import correlations
 
 from . import _version
 __version__ = _version.get_versions()['version']
