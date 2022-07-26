8db706e9dfaf197a32ab32ed1c7e33f37776182b
Anatoli Ulmer <anatoli.ulmer@gmail.com>
2022-07-26 08:22:59 -0700

    Add conversion from element symbol to atomic number and vice versa Change input for nist data to element symbold instead of atomic number

--- diffractionimaging/atomicformfactors.py
+++ diffractionimaging/atomicformfactors.py
@@ -1,16 +1,36 @@
+
+# This file is part of diffractionimaging.
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+# Copyright (C) 2022 Stephan Kuschel, Anatoli Ulmer
+
 import functools
 import requests
 from io import StringIO
 from bs4 import BeautifulSoup
 from urllib.parse import urljoin
 import numpy as np
+from scipy.constants import physical_constants as const
+
 
 
 __all__ = ['atomicformfactor_henke', 'atomicformfactor_nist', 'ev2lambda',
            'sigmascatt', 'sigmaabsorb']
 
 
-def atomicformfactor_henke(element):
+def atomic_form_factor_henke(element):
     '''
     atomic form factor from henke in complex form.
 
@@ -33,13 +53,14 @@ def _download_henke(element):
 
 
 @functools.cache
-def atomicformfactor_nist(Z):
+def atomic_form_factor_nist(element):
     '''
     atomic form factor from nist in complex form.
 
     returns:
       eV, f
     '''
+    Z = element2atomic_number(element)
     # baseurl = 'https://physics.nist.gov/cgi-bin'
     urltmplate = 'https://physics.nist.gov/cgi-bin/ffast/ffast.pl?gtype=4&Z={Z}'
     url = urltmplate.format(Z=Z)
@@ -55,14 +76,14 @@ def atomicformfactor_nist(Z):
     return eV, f
 
 
-def ev2lambda(ev):
+def ev2wavelength(ev):
     '''
-    returns lamdba in meter.
+    returns wavelength in meter.
     '''
     return 1.2398/ev/1e6
 
 
-def sigmascatt(f):
+def scattering_cross_section(f):
     '''
     converts the complext atomic form factor f to the real valued
     scattering cross section in m**2.
@@ -70,8 +91,50 @@ def sigmascatt(f):
     return 6.65e-29 * np.abs(f)**2
 
 
-def sigmaabsorb(f, lamb=1):
+def absorption_cross_section(f, lamb=1):
     '''
     lamb is the wavelength lambda in meter.
     '''
     return -2*2.8e-15 * lamb * np.imag(f)
+
+
+def atomic_number2element(atomic_number):
+    '''
+    returns element symbol for a given atomic number
+    '''
+    symbols = ['H','He','Li','Be','B','C','N','O','F','Ne',
+           'Na','Mg','Al','Si','P','S','Cl','Ar','K', 'Ca',
+           'Sc', 'Ti', 'V','Cr', 'Mn', 'Fe', 'Co', 'Ni',
+           'Cu', 'Zn', 'Ga', 'Ge', 'As', 'Se', 'Br', 'Kr',
+           'Rb', 'Sr', 'Y', 'Zr', 'Nb', 'Mo', 'Tc', 'Ru',
+           'Rh', 'Pd', 'Ag', 'Cd', 'In', 'Sn', 'Sb', 'Te',
+           'I', 'Xe','Cs', 'Ba','La', 'Ce', 'Pr', 'Nd', 'Pm',
+           'Sm', 'Eu', 'Gd', 'Tb', 'Dy', 'Ho', 'Er', 'Tm',
+           'Yb', 'Lu', 'Hf', 'Ta', 'W', 'Re', 'Os', 'Ir',
+           'Pt', 'Au', 'Hg', 'Tl', 'Pb', 'Bi', 'Po', 'At', 'Rn',
+           'Fr', 'Ra', 'Ac', 'Th', 'Pa', 'U', 'Np', 'Pu', 'Am',
+           'Cm', 'Bk', 'Cf', 'Es', 'Fm', 'Md', 'No', 'Lr',
+           'Rf', 'Db', 'Sg', 'Bh','Hs', 'Mt', 'Ds', 'Rg', 'Cn',
+           'Nh', 'Fl', 'Mc', 'Lv', 'Ts', 'Og']
+    return symbols[atomic_number-1]
+
+
+def element2atomic_number(element):
+    '''
+    returns atomic number for a given element symbol
+    '''
+    symbols = ['H','He','Li','Be','B','C','N','O','F','Ne',
+           'Na','Mg','Al','Si','P','S','Cl','Ar','K', 'Ca',
+           'Sc', 'Ti', 'V','Cr', 'Mn', 'Fe', 'Co', 'Ni',
+           'Cu', 'Zn', 'Ga', 'Ge', 'As', 'Se', 'Br', 'Kr',
+           'Rb', 'Sr', 'Y', 'Zr', 'Nb', 'Mo', 'Tc', 'Ru',
+           'Rh', 'Pd', 'Ag', 'Cd', 'In', 'Sn', 'Sb', 'Te',
+           'I', 'Xe','Cs', 'Ba','La', 'Ce', 'Pr', 'Nd', 'Pm',
+           'Sm', 'Eu', 'Gd', 'Tb', 'Dy', 'Ho', 'Er', 'Tm',
+           'Yb', 'Lu', 'Hf', 'Ta', 'W', 'Re', 'Os', 'Ir',
+           'Pt', 'Au', 'Hg', 'Tl', 'Pb', 'Bi', 'Po', 'At', 'Rn',
+           'Fr', 'Ra', 'Ac', 'Th', 'Pa', 'U', 'Np', 'Pu', 'Am',
+           'Cm', 'Bk', 'Cf', 'Es', 'Fm', 'Md', 'No', 'Lr',
+           'Rf', 'Db', 'Sg', 'Bh','Hs', 'Mt', 'Ds', 'Rg', 'Cn',
+           'Nh', 'Fl', 'Mc', 'Lv', 'Ts', 'Og']
+    return symbols.index(element)+1
